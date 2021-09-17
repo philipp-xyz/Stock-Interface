9e8cb70a06b800516362b04454cc6e31d6183fc0
AsuraX <vpzasurax@gmail.com>
2021-09-17 10:32:35 +0200

    Add Project

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Stock-Interface
-A basic stock interface
+# Stock Interface (stock-interface)
+
+This is a simple stock interface.
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://v2.quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '/', component: () => import('pages/Index.vue') },
+      { path: '/dividends', component: () => import('pages/Dividends.vue') },
+      { path: '/stocks', component: () => import('pages/Stocks.vue') },
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
